cea598a5f6a7a69707d3278b2e71404d5e280fa1
Mihai Costea <mihai.andrei.costea@icloud.com>
2019-02-18 22:08:58 +0200

    Added tests for yaml unmarshal

--- cmd/conductor/main.go
+++ cmd/conductor/main.go
@@ -1,14 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"github.com/CultOfAgileLimericks/conductor/internal/pkg/model"
 	"github.com/CultOfAgileLimericks/conductor/internal/pkg/plugin"
 	"github.com/CultOfAgileLimericks/conductor/internal/pkg/plugin/input"
 	"github.com/CultOfAgileLimericks/conductor/internal/pkg/plugin/output"
 	"github.com/sirupsen/logrus"
-	"gopkg.in/yaml.v2"
-	"io/ioutil"
 	"os"
 	"reflect"
 )
@@ -17,30 +13,11 @@ func InitPluginManager() {
 	plugin.Manager.RegisterInput("cron", input.NewCronInput, reflect.TypeOf(input.CronInputConfig{}))
 	plugin.Manager.RegisterInput("http", input.NewHTTPInput, reflect.TypeOf(input.HTTPInputConfig{}))
 	plugin.Manager.RegisterOutput("http", output.NewHTTPOutput, reflect.TypeOf(output.HTTPOutputConfig{}))
-
 }
 
 func main() {
-	logrus.SetOutput(os.Stdout)
-	logrus.SetLevel(logrus.DebugLevel)
-
 	InitPluginManager()
 
-	data, err := ioutil.ReadFile("internal/pkg/model/test_data/test_task1.yml")
-	if err != nil {
-		logrus.Error(err)
-	}
-
-	task := model.NewTask()
-
-	yamlError := yaml.Unmarshal(data, task)
-	if yamlError != nil {
-		logrus.Error(yamlError)
-	}
-
-	logrus.Info(task)
-
-	task.Run()
-
-	fmt.Printf("%+v", task)
+	logrus.SetOutput(os.Stdout)
+	logrus.SetLevel(logrus.DebugLevel)
 }
